7509444f50e8c63fc74cc1e2186cde792551ae98
Jhonatas R Correa <jhonatasrcorrea@gmail.com>
2024-12-11 20:56:46 -0300

    Criando botão de fechar e finalizando FrontEnd

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,14 +39,19 @@
             this.label3 = new System.Windows.Forms.Label();
             this.lblResultado = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Image = global::windows_form_study_project.Properties.Resources.png_transparent_computer_icons_simple_calculator_android_application_package_icon_calculator_svg_blue_electronics_electric_blue1;
-            this.pictureBox1.Location = new System.Drawing.Point(172, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(177, 35);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(162, 162);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -63,7 +68,7 @@
             this.btnCalc.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCalc.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCalc.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnCalc.Location = new System.Drawing.Point(271, 423);
+            this.btnCalc.Location = new System.Drawing.Point(253, 20);
             this.btnCalc.Name = "btnCalc";
             this.btnCalc.Size = new System.Drawing.Size(125, 31);
             this.btnCalc.TabIndex = 1;
@@ -73,7 +78,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(153, 214);
+            this.label1.Location = new System.Drawing.Point(158, 235);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(28, 13);
             this.label1.TabIndex = 2;
@@ -84,7 +89,7 @@
             this.txtNumero1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtNumero1.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.txtNumero1.Location = new System.Drawing.Point(194, 211);
+            this.txtNumero1.Location = new System.Drawing.Point(199, 232);
             this.txtNumero1.Name = "txtNumero1";
             this.txtNumero1.Size = new System.Drawing.Size(125, 20);
             this.txtNumero1.TabIndex = 3;
@@ -103,7 +108,7 @@
             "Soma",
             "Multiplicação",
             "Subtração"});
-            this.cboTipoConta.Location = new System.Drawing.Point(194, 292);
+            this.cboTipoConta.Location = new System.Drawing.Point(199, 313);
             this.cboTipoConta.Name = "cboTipoConta";
             this.cboTipoConta.Size = new System.Drawing.Size(126, 21);
             this.cboTipoConta.TabIndex = 6;
@@ -112,7 +117,7 @@
             // 
             this.txtNumero2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNumero2.Location = new System.Drawing.Point(194, 251);
+            this.txtNumero2.Location = new System.Drawing.Point(199, 272);
             this.txtNumero2.Name = "txtNumero2";
             this.txtNumero2.Size = new System.Drawing.Size(125, 20);
             this.txtNumero2.TabIndex = 8;
@@ -120,7 +125,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(153, 254);
+            this.label2.Location = new System.Drawing.Point(158, 275);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 7;
@@ -136,7 +141,7 @@
             this.btnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLimpar.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLimpar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnLimpar.Location = new System.Drawing.Point(122, 423);
+            this.btnLimpar.Location = new System.Drawing.Point(122, 20);
             this.btnLimpar.Name = "btnLimpar";
             this.btnLimpar.Size = new System.Drawing.Size(125, 31);
             this.btnLimpar.TabIndex = 9;
@@ -146,7 +151,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(119, 295);
+            this.label3.Location = new System.Drawing.Point(124, 316);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(69, 13);
             this.label3.TabIndex = 10;
@@ -164,11 +169,51 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.btnLimpar);
+            this.panel1.Controls.Add(this.btnCalc);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 390);
+            this.panel1.Location = new System.Drawing.Point(0, 408);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(503, 100);
+            this.panel1.Size = new System.Drawing.Size(503, 82);
             this.panel1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(444, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Fechar";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(503, 29);
+            this.panel2.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Calculadora";
             // 
             // Form1
             // 
@@ -178,19 +223,23 @@
             this.ClientSize = new System.Drawing.Size(503, 490);
             this.Controls.Add(this.lblResultado);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnLimpar);
             this.Controls.Add(this.txtNumero2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cboTipoConta);
             this.Controls.Add(this.txtNumero1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnCalc);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,6 +258,9 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
     }
 }
 
